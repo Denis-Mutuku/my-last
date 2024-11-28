17200ee6d82751f61fd09831314bdc4806ad8ad1
Denis-Mutuku <n.denismutuku@gmail.com>
2024-11-28 15:39:22 +0000

    initial analysis

--- mylast.R
+++ mylast.R
@@ -1,1 +1,19 @@
-###
+### Loading the packages
+
+library(tidyverse)
+
+# data
+
+starwars <- starwars
+
+# EDA 
+
+glimpse(starwars)
+
+dim(starwars)
+
+# filtering and selecting
+
+starwars %>% 
+  select(name, height, mass, sex, species) %>% 
+  filter(sex %in% c("male", "female"), species == "Human")
