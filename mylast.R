64e0c474cf1479b15e326fd1044a214b611e81f4
Denis-Mutuku <n.denismutuku@gmail.com>
2024-11-28 15:57:22 +0000

    bmi calculation, study recruits

--- mylast.R
+++ mylast.R
@@ -14,6 +14,25 @@ dim(starwars)
 
 # filtering and selecting
 
-starwars %>% 
+data1 <- starwars %>% 
   select(name, height, mass, sex, species) %>% 
   filter(sex %in% c("male", "female"), species == "Human")
+
+# bmi calc
+
+data2 <- data1 %>% 
+  drop_na(height, mass) %>% 
+  mutate(height = height/100,
+         bmi = mass/height^2,
+         weight = case_when(bmi < 18 ~ "underweight",
+                            bmi > 18 & bmi < 25 ~ "normal",
+                            bmi > 25 & bmi < 30 ~ "overweight",
+                            bmi > 30 ~ "obese"))
+# study recruitment
+
+data2 %>% 
+  mutate(participant = ifelse(weight == "normal",
+                          "yes", "no")) %>% 
+  group_by(participant) %>% 
+  summarise(number = n())
+
